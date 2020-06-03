2b948c7ab4c6e700cbcd3fcd65e9ce83786f2c06
Nikolay Deriglazov <nikinik@inbox.ru>
2020-06-04 01:01:38 +0600

    Make migrations for Flight and User entities

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -10,11 +10,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UsersFixtures extends Fixture
 {
     private $passwordEncoder;
-    
-    public const MODERATOR = 'moder';
-    public const SELLER = 'seller';
-    public const CUSTOMER = 'customer';
-
+   
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
---
+++ src/Migrations/Version20200603184107.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200603184107 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+    
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE flight_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('CREATE TABLE flight (id INT NOT NULL, from_city VARCHAR(255) NOT NULL, to_city VARCHAR(255) NOT NULL, seats_count INT NOT NULL, date DATE NOT NULL, status SMALLINT NOT NULL, PRIMARY KEY(id))');
+    }
+    
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE flight_id_seq CASCADE');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE flight');
+    }
+}
